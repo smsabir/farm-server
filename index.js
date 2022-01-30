a3c965ee88e7c9c71bb8785916d8800acb90841e
S M Abdullah Al Sabir <ssabir@ttu.ee>
2022-01-30 10:04:40 +0200

    installed mongoose, dotenv and configured them

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
+
+// DB connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctdrv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
     res.status(200).send({
