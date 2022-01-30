1cfa086e81c083855b90e817b57c7f43a3f41570
S M Abdullah Al Sabir <ssabir@ttu.ee>
2022-01-30 10:19:45 +0200

    server basic setup completed

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
+const fs = require('fs');
+const csv = require('csv-parser');
+const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
 
-const app = express();
+// Server Port
 const PORT = process.env.PORT || 4000;
 
-// DB connection
+// Express app initialization
+const app = express();
+app.use(express.json());
+
+
+// Database connection with mongoose
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctdrv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
@@ -16,12 +25,25 @@ mongoose
     .then(() => console.log('connection successful'))
     .catch((err) => console.log(err));
 
+// Server status check
 app.get("/", (req, res) => {
     res.status(200).send({
         status: 1,
         message: `🚀 App Running on port http://localhost:${PORT}`
-    })
-})
+    });
+});
+
+// Application routes
+app.use();
+
+
+// Default error handler
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+      return next(err);
+    }
+    res.status(500).json({ error: err });
+};
 
 app.listen(PORT, () => {
     console.log(`app listening at port ${PORT}`);
