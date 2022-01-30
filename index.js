7035fad95e54ae7462a00bc9ac72fbf37d62312f
S M Abdullah Al Sabir <ssabir@ttu.ee>
2022-01-30 10:10:45 +0200

    installed cors, csv-parser, express fileupload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
-const mongoose = require('mongoose');
 
 // DB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctdrv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
